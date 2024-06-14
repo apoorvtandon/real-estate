50c913bb64215185d30adb521b83bb73c540ed92
Aryan Jaiswal <theguruaryan@gmail.com>
2024-06-14 13:31:50 +0530

    sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,23 @@
-import React from "react"
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import Signin from "./pages/Signin"
-import Home from "./pages/Home"   
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Header from "./components/Header"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Header from "./components/Header";
 function App() {
-
-
   return (
     <>
-       <BrowserRouter>
-        <Header/> 
-         <Routes>
-          <Route path ='/' element={<Home/>}></Route>
-          <Route path ='/sign-in' element={<Signin/>}></Route>
-          <Route path ='/sign-up' element={<SignUp/>}></Route>
-          <Route path ='/about' element={<About/>}></Route>
-         </Routes>
-       </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/sign-in" element={<Signin />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
