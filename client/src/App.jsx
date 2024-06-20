87e8b833fda38aa24c968ed14180dd64dc4a44a5
Aryan Jaiswal <theguruaryan@gmail.com>
2024-06-21 04:26:43 +0530

    complete update listing function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signin from "./pages/Signin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
-import Header from "./components/Header";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-function App() {
+import UpdateListing from "./pages/UpdateListing";
+
+export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/sign-in" element={<Signin />}></Route>
-          <Route path="/sign-up" element={<SignUp />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/create-listing" element={<CreateListing />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
