95c211a5036306261b8eeff2d520568a62b61e89
Apoorv <apoorvtandon814@gmail.com>
2024-07-06 13:13:36 +0530

    some changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from './routes/listing.route.js';
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -17,7 +18,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-   
+  const __dirname = path.resolve();
 app.listen(3000, () => {
   console.log("Server is running on port 3000 !!");
 });
@@ -25,6 +26,14 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
